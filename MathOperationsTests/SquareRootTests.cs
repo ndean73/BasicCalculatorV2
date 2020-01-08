b622e38e4c8ce854a46e10f573e5bb0c6d717601
ndean73 <Dean.Nalumpa@mlnr.gov.zm>
2020-01-08 04:27:34 -0800

    Task: Added MathOperations Librarys my Basic Calculator on GitHub

---
+++ MathOperationsTests/SquareRootTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathOperations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathOperations.Tests
+{
+    [TestClass()]
+    public class SquareRootTests
+    {
+
+        //private readonly double b = 4;
+        Random b = new Random();
+
+        [TestMethod()]
+        public void SqrRootTest()
+        {
+            Assert.AreEqual(2, SquareRoot.SqrRoot(b.Next(1,50)));
+        }
+
+        
+    }
+}
